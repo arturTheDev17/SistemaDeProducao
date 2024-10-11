5df64cd886250623a4f3ed0925a51a5ea815192b
Pedro Augusto Wilhelm <147540805+Hivqs79@users.noreply.github.com>
2024-10-11 15:22:38 -0300

    :sparkles: feat: Finishing EmployeePanel with managers visualization

--- src/panel/EmployeePanel.java
+++ src/panel/EmployeePanel.java
@@ -18,6 +18,7 @@ public class EmployeePanel implements Observer {
     // Map to store the log errors of employees, keyed by machine name
     private static final Map<String, DataEmployee> EMPLOYEES_LOG_ERRORS = new HashMap<>();
 
+    private final static ArrayList<String> MACHINES_WITH_PROBLEMS = new ArrayList<>();
     // Flag to track if the UI screen is active
     private static boolean screenActive = false;
 
@@ -58,8 +59,18 @@ public class EmployeePanel implements Observer {
             // Update the count of machines with problems
             if (oldData.getProblems().isEmpty() && !dataEmployee.getProblems().isEmpty()) {
                 amountOfMachinesWthProblems++;
+                if (!MACHINES_WITH_PROBLEMS.contains(dataEmployee.getMachineName())) {
+                    MACHINES_WITH_PROBLEMS.add(dataEmployee.getMachineName());
+                }
             } else if (!oldData.getProblems().isEmpty() && dataEmployee.getProblems().isEmpty()) {
                 amountOfMachinesWthProblems--;
+                MACHINES_WITH_PROBLEMS.remove(dataEmployee.getMachineName());
+            }
+        } else {
+            if (dataEmployee.getProblems().size() > 0) {
+                amountOfProblems += dataEmployee.getProblems().size();
+                amountOfMachinesWthProblems++;
+                MACHINES_WITH_PROBLEMS.add(dataEmployee.getMachineName());
             }
         }
 
@@ -80,8 +91,15 @@ public class EmployeePanel implements Observer {
         }
     }
 
-    // JList to display the error logs
+    /**
+     * JList to display the error logs
+     */
     private static final JList<String> LOG_ERRORS_LIST = new JList<>();
+
+    /**
+     * JList to display the error logs
+     */
+    private static final JList<String> MACHINES_WITH_PROBLEMS_LIST = new JList<>();
 
     /**
      * Creates and displays the user interface for the Employee Panel.
@@ -97,7 +115,7 @@ public class EmployeePanel implements Observer {
         JLabel erroManager = new JLabel();
 
         // Set up the log errors list model
-        LOG_ERRORS_LIST.setModel(getLogData());
+        LOG_ERRORS_LIST.setModel(getLogData("Employee"));
         LOG_ERRORS_LIST.setCellRenderer(new MyListCellRenderer());
 
         LOG_ERRORS_LIST.setBackground(new java.awt.Color(220, 220, 255));
@@ -140,14 +158,28 @@ public class EmployeePanel implements Observer {
         tituloManager.setFont(tituloManager.getFont().deriveFont(24.0f));
         tituloManager.setBounds((700 - tituloManager.getPreferredSize().width) / 2, 20, 300, 50);
 
+        labelCountMachinesWithProblems.setBounds(50, 100, 250, labelCountMachinesWithProblems.getPreferredSize().height);
+        labelAmountOfProblems.setBounds(50, 140, 250, labelAmountOfProblems.getPreferredSize().height);
 
-        labelCountMachinesWithProblems.setBounds(50, 80, labelCountMachinesWithProblems.getPreferredSize().width, labelCountMachinesWithProblems.getPreferredSize().height);
-        labelAmountOfProblems.setBounds(50, 120, labelAmountOfProblems.getPreferredSize().width, labelAmountOfProblems.getPreferredSize().height);
+        JLabel titleListManagers = new JLabel("Machines with problems: ");
+        titleListManagers.setFont(tituloManager.getFont().deriveFont(18.0f));
+        titleListManagers.setBounds(50, 180, 300, 50);
+
+        // Set up the log errors list model
+        MACHINES_WITH_PROBLEMS_LIST.setModel(getLogData("Manager"));
+
+        MACHINES_WITH_PROBLEMS_LIST.setBackground(new java.awt.Color(220, 220, 255));
+        JScrollPane scrollPanelManagers = new JScrollPane();
+        scrollPanelManagers.setBounds(50, 250, 580, 400);
+        scrollPanelManagers.setViewportView(MACHINES_WITH_PROBLEMS_LIST);
+        scrollPanelManagers.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
         // Add components to the managers panel
         panelManager.add(tituloManager);
         panelManager.add(labelCountMachinesWithProblems);
         panelManager.add(labelAmountOfProblems);
+        panelManager.add(titleListManagers);
+        panelManager.add(scrollPanelManagers);
         panelManager.add(erroManager);
 
         // Add tabs to the dialog
@@ -163,10 +195,11 @@ public class EmployeePanel implements Observer {
      * Updates the data displayed in the log errors list.
      */
     private void updateDataScreen() {
-        LOG_ERRORS_LIST.setModel(getLogData());
+        LOG_ERRORS_LIST.setModel(getLogData("Employee"));
         LOG_ERRORS_LIST.setCellRenderer(new MyListCellRenderer());
         labelAmountOfProblems.setText("Amount of problems: " + amountOfProblems);
         labelCountMachinesWithProblems.setText("Machines with problems: " + amountOfMachinesWthProblems);
+        MACHINES_WITH_PROBLEMS_LIST.setModel(getLogData("Manager"));
     }
 
     /**
@@ -174,12 +207,17 @@ public class EmployeePanel implements Observer {
      *
      * @return A DefaultListModel containing the log entries.
      */
-    private DefaultListModel getLogData() {
+    private DefaultListModel getLogData(String type) {
         DefaultListModel listModel = new DefaultListModel<>();
-
-        for (Map.Entry<String, DataEmployee> entry : EMPLOYEES_LOG_ERRORS.entrySet()) {
-            DataEmployee value = entry.getValue();
-            listModel.addElement(value);
+        if (type.equals("Employee")) {
+            for (Map.Entry<String, DataEmployee> entry : EMPLOYEES_LOG_ERRORS.entrySet()) {
+                DataEmployee value = entry.getValue();
+                listModel.addElement(value);
+            }
+        } else if (type.equals("Manager")) {
+            for (String machineName : MACHINES_WITH_PROBLEMS) {
+                listModel.addElement(machineName);
+            }
         }
         return listModel;
     }
